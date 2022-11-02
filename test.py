1a25bc3ee3b0b6d6060028f4104c592bdb954fd1
JackZhang9 <52847412+JackZhang9@users.noreply.github.com>
2022-11-02 17:55:25 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+'''
+@Author  : JackZhang9
+@Time    : 2022/11/2 15:35
+'''
+
+import cv2
+from matplotlib import pyplot as plt
+
+img=cv2.imread("F:\CV\yolo\yolov5-v5.0\yolov5-v5.0\data\images\zidane.jpg")
+cv2.imshow('zidane',img)
+cv2.waitKey(500)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
